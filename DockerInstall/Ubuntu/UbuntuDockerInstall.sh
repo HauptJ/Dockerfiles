a0c1cbe76e80782d6356435968f592bac240ea4f
Joshua Haupt <joshua@hauptj.com>
2018-01-05 20:29:46 -0600

    Non-root access to docker debug

--- DockerInstall/Ubuntu/UbuntuDockerInstall.sh
+++ DockerInstall/Ubuntu/UbuntuDockerInstall.sh
@@ -61,7 +61,7 @@ sudo apt-get install docker-ce
 # Source: https://askubuntu.com/questions/477551/how-can-i-use-docker-without-sudo
 
 # Add the docker group if it doesn't already exist:
-sudo groupadd docker
+#sudo groupadd docker
 
 # Add the connected user "$USER" to the docker group. Change the user name to match your preferred user if you do not want to use your current user:
 sudo gpasswd -a $USER docker
