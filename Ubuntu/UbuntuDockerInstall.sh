dbe1ffbcc97a92fed53ce7a3f8e73771f29229a5
Joshua Haupt <joshua@hauptj.com>
2018-01-05 18:37:50 -0600

    Debug Ubuntu install script

--- Ubuntu/UbuntuDockerInstall.sh
+++ Ubuntu/UbuntuDockerInstall.sh
@@ -32,11 +32,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 ## TODO auto verification ##
 echo "Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, by searching for the last 8 characters of the fingerprint."
 echo "Y/N?"
-read userInput
+read $userInput
 
 # Abort on N continue if Y
 # ERROR: ./UbuntuDockerInstall.sh: line 38: [Y: command not found
-if ["$userInput" != "Y"]
+if [$userInput != "Y"]
 then
 	exit
 fi
